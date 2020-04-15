a7f0b06b9bd085aa4c0d369a85c1626995c3396c
Dave <DaveBathnes@users.noreply.github.com>
2020-04-15 07:30:46 +0100

    General layout and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BrowserRouter, Link, Route } from "react-router-dom";
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
@@ -10,13 +12,13 @@ import Typography from '@material-ui/core/Typography';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { makeStyles } from '@material-ui/core/styles';
-import blue from '@material-ui/core/colors/blue';
-import yellow from '@material-ui/core/colors/yellow';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import blueGrey from '@material-ui/core/colors/blueGrey';
 
 const theme = createMuiTheme({
   palette: {
-    primary: blue,
-    secondary: yellow
+    primary: lightBlue,
+    secondary: blueGrey
   },
   overrides: {
     MuiButton: {
@@ -35,6 +37,13 @@ const theme = createMuiTheme({
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  },
+  title: {
+    margin: theme.spacing(3)
   }
 }));
 
@@ -44,17 +53,35 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <CssBaseline />
-        <Container maxWidth="lg">
-          <Typography color="primary" variant="h1">Libraries at home</Typography>
+      <BrowserRouter>
+        <div className={classes.root}>
+          <CssBaseline />
+          <Container maxWidth="lg">
+            <Typography color="secondary" variant="h4" component="h1" className={classes.title}>Libraries at home</Typography>
+          </Container>
           <AppBar position="static" color="default" elevation={0}>
-            <Toolbar>
-              <Button color="primary">Login</Button>
-            </Toolbar>
+            <Container maxWidth="lg">
+              <Toolbar>
+                <Button size="large" component={Link} to="/" color="primary" onClick={() => { }}>Home</Button>
+                <Button size="large" component={Link} to="/watch" color="primary" onClick={() => { }}>Watch</Button>
+                <Button size="large" component={Link} to="/blogs" color="primary" onClick={() => { }}>Read</Button>
+              </Toolbar>
+            </Container>
           </AppBar>
-        </Container>
-      </div>
+
+          <Container maxWidth="lg">
+            <main className={classes.content}>
+              <Route
+                path='/'
+                exact
+                render={() => {
+                  return <div>Home</div>
+                }}
+              />
+            </main>
+          </Container>
+        </div>
+      </BrowserRouter>
     </ThemeProvider >
   );
 }
