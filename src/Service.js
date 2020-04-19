494c35143327890504b59bf0dd916674becfc534
Dave <DaveBathnes@users.noreply.github.com>
2020-04-19 11:51:41 +0100

    Librsry services search data

--- src/Service.js
+++ src/Service.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import Button from '@material-ui/core/Button';
-import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
-import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -17,7 +17,20 @@ const config = require('./helpers/config.json');
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingTop: '20px'
+    paddingTop: theme.spacing(2)
+  },
+  serviceName: {
+    fontWeight: 500
+  },
+  leadButton: {
+    marginTop: theme.spacing(1)
+  },
+  linkContainer: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(3)
+  },
+  linkText: {
+
   }
 }));
 
@@ -28,19 +41,29 @@ function Service(props) {
 
   return (
     <div className={classes.root}>
-      <Typography component="h2" variant="h6" color="secondary" className={classes.subtitle}>{'Your local library service is ' + service.Name}</Typography>
+      <Typography component="h2" variant="h5" color="secondary">{'Your local library service is '}<span className={classes.serviceName}>{service.Name}</span></Typography>
       <br />
       <Grid container spacing={3}>
-        {config.services_text.library_service.map((library, idx) => {
-          return <Grid key={'grd_usingurls_' + idx} item xs={12} sm={6} md={4} lg={4} xl={3}>
+        {config.services_text.library_service.filter(s => s.field in service).map((library, idx) => {
+          return <Grid key={'grd_service_' + idx} item xs={12} sm={6} md={4} lg={4} xl={3}>
             <Alert severity="info">
               <AlertTitle>{library.title.replace('[service]', service.Name)}</AlertTitle>
               {library.description}<br />
-              <Button color="secondary" startIcon={<OpenInNewIcon />} target="_blank" href={service[library.field]}>{library.link_text}</Button>
+              <Button className={classes.leadButton} variant="outlined" size="large" color="primary" startIcon={<OpenInNewIcon />} target="_blank" href={service[library.field]}>{library.link_text}</Button>
             </Alert>
           </Grid>
         })}
       </Grid>
+      <Paper variant="outlined" className={classes.linkContainer}>
+        <Typography component="h3" variant="h6" color="secondary">Useful links</Typography>
+        <Typography component="p" variant="body1" className={classes.linkText}>
+        {config.services_text.service_links.filter(s => s.field in service).map((link, idx) => {
+          return <React.Fragment>
+            <Link key={'typ_link_' + idx} href={service[link.field]} variant="body1">{link.text}</Link><br />
+          </React.Fragment>
+        })}
+        </Typography>
+      </Paper>
     </div>
   );
 }
--- src/Watch.js
+++ src/Watch.js
@@ -6,7 +6,6 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Dialog from '@material-ui/core/Dialog';
-import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import Typography from '@material-ui/core/Typography';
@@ -114,8 +113,6 @@ function Watch(props) {
               </Grid>
             })}
           </Grid>
-          <br />
-          <Divider />
           <br />
         </React.Fragment>
       })}
